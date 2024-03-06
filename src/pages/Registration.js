048c63a47eee208878b6d0d4f8b016883c19288c
Shivaprasad2625 <113909979+Shivaprasad2625@users.noreply.github.com>
2024-03-06 12:33:49 +0530

    Add files via upload

---
+++ src/pages/Registration.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+export default function Registration() {
+  return (
+    <div>
+      <h2>Registration Form</h2>
+
+      <form action="register" method="post">
+        <label>Username:</label>
+        <br />
+        <input type="text" name="username" />
+        <br />
+        <label>Email:</label>
+        <br />
+        <input type="email" name="email" />
+        <br />
+        <label>Password:</label>
+        <br />
+        <input type="password" name="password" />
+        <br />
+        <label>Gender:</label>
+        <br />
+        Male
+        <input type="radio" name="gender" value="male" />
+        Female
+        <input type="radio" name="gender" value="female" />
+        Other
+        <input type="radio" name="gender" value="other" />
+        <br />
+        <label>Role:</label>
+        <br />
+        Admin
+        <input type="radio" name="role" value="admin" />
+        Customer
+        <input type="radio" name="role" value="customer" />
+        <br />
+        <label>Address:</label>
+        <br />
+        <textarea name="address"></textarea>
+        <br />
+        <input type="submit" value="SUBMIT" />
+      </form>
+    </div>
+  );
+}
